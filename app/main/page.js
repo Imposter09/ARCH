da8e2f8428c71756d5fa804374c522ad00de1858
Imposter09 <malharpatil0@gmail.com>
2025-09-09 00:11:26 +0530

    WIP: saving my changes before rebase

--- app/main/page.js
+++ app/main/page.js
@@ -552,10 +552,10 @@ const inputClass =
   <button
               type="button"
               onClick={handleSubmit}
-              className="bg-blue-500 text-white px-6 py-3 rounded-xl font-semibold shadow-md hover:bg-red-600 hover:shadow-lg transition duration-300 transform hover:-translate-y-1"
+              className="bg-blue-500 text-white px-6 py-3 rounded-xl font-semibold shadow-md hover:bg-blue-600 hover:shadow-lg transition duration-300 transform hover:-translate-y-1"
               disabled={pdfLoading}
             >
-              {pdfLoading ? "Submit" : "Submitting"}
+              {pdfLoading ? "Submitting" : "Submit"}
             </button>
 </div>
             {/* ADD THIS BUTTON: PDF Generation */}
